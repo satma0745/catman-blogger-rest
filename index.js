f5f1190f39700348726483affe31abb3435896a3
catman0745 <catman0745@gmail.com>
2021-04-27 19:21:52 +0300

    Configure dotenv

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express')
+const dotenv = require('dotenv')
 
+dotenv.config()
 const app = express()
 
 app.get('/', (_, res) => {
   res.send('catman-blogger-rest')
 })
 
-const port = 5000
+const port = process.env.PORT
 app.listen(port, () => {
   console.log(`Running on port ${port}`)
 })
